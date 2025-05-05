43b7774f07d7e6d4f4e10b7964d721bcb8bbce03
denis-gandel <gandarillas.delgado.denis@gmail.com>
2025-05-05 07:28:23 -0400

    fix: remove roles and permissions

--- Domain/Entities/Concretes/Role.cs
+++
@@ -1,9 +0,0 @@
-using UserService.Domain.Entities.Abstracts;
-
-namespace UserService.Domain.Entities.Concretes
-{
-    public class Role : Entity<int>
-    {
-        public string Name { get; set; } = "";
-    }
-}
